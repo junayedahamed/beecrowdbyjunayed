db7d0c50287a703029b2cc0bfe48402423312d6f
junayedahamed <junayedahamed660@mail.com>
2022-10-12 18:43:31 +0600

    rearrange

--- bcrwd2456.c
+++ bcrwd2456.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z,p,q,r;
-   scanf("%d%d%d%d%d%d",&x,&y,&z,&p,&q,&r);
-   if(x<y<z<p<q<r){
-    printf("C\n");
-   }
-   else if(x>y>z>p>q>r){
-    printf("D\n");
-   }
-   else{
-    printf("N\n");
-   }
-   return 0;
+
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+
+    if(a<b&&b<c&&c<d&&d<e){
+        printf("C\n");
+    }
+    else if(a>b&&b>c&&c>d&&d>e){
+        printf("D\n");
+    }
+    else{
+       printf("N\n");
+}
+return 0;
 }
