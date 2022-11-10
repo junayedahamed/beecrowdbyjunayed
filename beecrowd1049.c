53f355855c03715117d184bf970935d578485cc1
junayedahamed <junayedahamed660@mail.com>
2022-11-10 23:07:38 +0600

    solved

---
+++ beecrowd1049.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    char a[10],b[10],c[10];
+
+    scanf("%s %s %s",&a,&b,&c);
+
+    if(a[0]=='v' &&b[0]=='a'&&c[0]=='c'){
+        printf("aguia\n");
+    }
+    if(a[0]=='v' &&b[0]=='a'&&c[0]=='o'){
+        printf("pomba\n");
+    }
+    if(a[0]=='v' &&b[0]=='m'&&c[0]=='o'){
+        printf("homem\n");
+    }
+    if(a[0]=='v' &&b[0]=='m'&&c[0]=='h'){
+        printf("vaca\n");
+
+    }
+    if(a[0]=='i' &&b[0]=='i'&&c[2]=='m'){
+        printf("pulga\n");
+    }
+      if(a[0]=='i' &&b[0]=='i'&&c[2]=='r'){
+        printf("lagarta\n");
+    }
+
+      if(a[0]=='i' &&b[0]=='a'&&c[0]=='h'){
+        printf("sanguessuga\n");
+    }
+      if(a[0]=='i' &&b[0]=='a'&&c[0]=='o'){
+        printf("minhoca\n");
+    }
+
+    return 0;
+}
